9b1763696ac6aeff44840195fbe616e0e7238c1b
boranb <berkayboran@gmail.com>
2020-06-16 18:03:32 +0300

    Efrepository

--- src/ApplicationCore/Interfaces/IAsyncRepository.cs
+++ src/ApplicationCore/Interfaces/IAsyncRepository.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using ApplicationCore.Entities;
 
@@ -7,5 +9,7 @@ namespace ApplicationCore.Interfaces
     public interface IAsyncRepository<T> where T : BaseEntity
     {
         Task<IReadOnlyList<T>> ListAllAsync();
+
+        Task<IReadOnlyList<T>> ListAsync(Expression<Func<T, bool>> predicate);
     }
 }
